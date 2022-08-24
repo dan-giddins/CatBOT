5618b9f2333ca80df326e18dd94dc0e00f486f31
Dan Giddins <daniel.giddins@nottingham.ac.uk>
2022-08-24 21:52:55 +0100

    faf

--- CatBot/Program.cs
+++ CatBot/Program.cs
@@ -6,42 +6,42 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace CatBot
 {
-    internal class Program
-    {
-        private static void Main(string[] args) =>
-            new Program().MainAsync().GetAwaiter().GetResult();
+	internal class Program
+	{
+		private static void Main(string[] args) =>
+			new Program().MainAsync().GetAwaiter().GetResult();
 
-        public async Task MainAsync()
-        {
-            using (var services = ConfigureServices())
-            {
-                var client = services.GetRequiredService<DiscordSocketClient>();
-                client.Log += LogAsync;
-                services.GetRequiredService<CommandService>().Log += LogAsync;
-                var waitForReady = new TaskCompletionSource<bool>();
-                client.Ready += async () => waitForReady.SetResult(true);
-                await client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("CatBotToken"));
-                await client.StartAsync();
-                await services.GetRequiredService<ListenService>().Initialize();
-                await services.GetRequiredService<SpeakService>().Initialize(waitForReady);
-                await Task.Delay(Timeout.Infinite);
-            }
-        }
+		public async Task MainAsync()
+		{
+			using (var services = ConfigureServices())
+			{
+				var client = services.GetRequiredService<DiscordSocketClient>();
+				client.Log += LogAsync;
+				services.GetRequiredService<CommandService>().Log += LogAsync;
+				var waitForReady = new TaskCompletionSource<bool>();
+				client.Ready += async () => waitForReady.SetResult(true);
+				await client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("CatBotToken"));
+				await client.StartAsync();
+				await services.GetRequiredService<ListenService>().Initialize();
+				await services.GetRequiredService<SpeakService>().Initialize(waitForReady);
+				await Task.Delay(Timeout.Infinite);
+			}
+		}
 
-        private Task LogAsync(LogMessage log)
-        {
-            Console.WriteLine(log.ToString());
-            return Task.CompletedTask;
-        }
+		private Task LogAsync(LogMessage log)
+		{
+			Console.WriteLine(log.ToString());
+			return Task.CompletedTask;
+		}
 
-        private ServiceProvider ConfigureServices() =>
-            new ServiceCollection()
-                .AddSingleton<DiscordSocketClient>()
-                .AddSingleton<CommandService>()
-                .AddSingleton<ListenService>()
-                .AddSingleton<SpeakService>()
-                .AddSingleton<HttpClient>()
-                .AddSingleton<PictureService>()
-                .BuildServiceProvider();
-    }
+		private ServiceProvider ConfigureServices() =>
+			new ServiceCollection()
+				.AddSingleton<DiscordSocketClient>()
+				.AddSingleton<CommandService>()
+				.AddSingleton<ListenService>()
+				.AddSingleton<SpeakService>()
+				.AddSingleton<HttpClient>()
+				.AddSingleton<PictureService>()
+				.BuildServiceProvider();
+	}
 }
